35ddedea2af7d5dc18daefd91844e825e0617eee
nate-l-beckwith <natebeckwith@proton.me>
2024-11-18 13:55:58 -0500

    Add Code_install.sh for Visual Studio Code installation on Linux

---
+++ Code_install.sh
@@ -0,0 +1,15 @@
+﻿#!/bin/bash
+# Install Visual Studio Code
+# https://code.visualstudio.com/docs/setup/linux
+
+sudo apt update && sudo apt upgrade -y
+sudo apt install -y wget gpg
+
+wget -qO- https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > microsoft.gpg
+
+sudo install -o root -g root -m 644 microsoft.gpg /usr/share/keyrings/
+
+sudo sh -c 'echo "deb [arch=amd64 signed-by=/usr/share/keyrings/microsoft.gpg] https://packages.microsoft.com/repos/vscode stable main" > /etc/apt/sources.list.d/vscode.list'
+
+sudo apt install code -y
+sudo apt autoremove -y
